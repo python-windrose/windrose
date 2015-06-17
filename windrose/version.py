f3799aba0e2efac040034ae543aebc7cbf4f3f47
scls19fr <s.celles@gmail.com>
2015-06-17 20:35:10 +0200

    Add version.py

---
+++ windrose/version.py
@@ -0,0 +1,7 @@
+__author__ = 'Lionel Roubeyrie'
+__credits__ = ["Sebastien Celles"]
+__mail__ = 's.celles@gmail.com'
+__version__ = '1.4'
+__license__ = 'CeCILL-B'
+__url__ = 'https://github.com/scls19fr/windrose'
+__maintainer__ = "Sebastien Celles"

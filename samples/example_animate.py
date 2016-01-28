fcd188e8f19bb8eba8ccb5829f7b4174524f1e42
scls19fr <s.celles@gmail.com>
2016-01-28 21:21:43 +0100

    Major fix to animate example

--- samples/example_animate.py
+++ samples/example_animate.py
@@ -22,6 +22,7 @@ import matplotlib.animation
 import matplotlib.cm as cm
 
 import pandas as pd
+pd.set_option('max_rows', 10)
 import numpy as np
 
 from windrose import (WindroseAxes, WindAxes, plot_windrose, 
@@ -29,11 +30,36 @@ from windrose import (WindroseAxes, WindAxes, plot_windrose,
 
 S_FIGSIZE_DEFAULT = ",".join(map(str, FIGSIZE_DEFAULT))
 
+def get_by_func(by=None, by_func=None):
+    if by is None and by_func is None:
+        by = 'MS'
+
+    if by in ['year', 'yearly', 'Y']:
+        return lambda dt: (dt.year, dt.month, dt.day)
+    elif by in ['month', 'monthly', 'MS']:  # MS: month start
+        return lambda dt: (dt.year, dt.month)
+    elif by in ['day', 'daily', 'D']:
+        return lambda dt: (dt.year, dt.month, dt.day)
+    elif by is None and func is not None:
+        return by_func
+    else:
+        raise NotImplementedError("'%s' is not an allowed 'by' parameter" % by)
+
+def generate(df_all, func, copy=True):
+    if copy:
+        df_all = df_all.copy()
+    df_all['by'] = df_all.index.map(func)
+    df = df_all.reset_index().set_index(['by', df_all.index.name])
+    for by_val in df.index.levels[0]:
+        yield df.loc[by_val]
+
+def count(df_all, func):
+    return len(np.unique(df_all.index.map(func)))
+
 @click.command()
 @click.option("--filename", default="samples/sample_wind_poitiers.csv", help="Input filename")
 @click.option("--exit_at", default=0, help="premature exit (int) - must be > 1")
-@click.option("--size", default=36 * 4, help="size of window")
-@click.option("--offset", default=3 + 6 * 6 + 24 * 6 * 365 * 2 - 24 * 6 * 65, help="data offset")
+@click.option("--by", default='month', help="Animate by (year, month, day...)")
 @click.option("--filename_out", default="windrose_animation.mp4", help="Output filename")
 @click.option("--dpi", default=DPI_DEFAULT, help="Dot per inch for plot generation")
 @click.option("--figsize", default=S_FIGSIZE_DEFAULT, help="Figure size x,y - default=%s" % S_FIGSIZE_DEFAULT)
@@ -42,22 +68,28 @@ S_FIGSIZE_DEFAULT = ",".join(map(str, FIGSIZE_DEFAULT))
 @click.option("--bins_max", default=20, help="Bins maximum value")
 @click.option("--bins_step", default=2, help="Bins step value")
 @click.option("--fontname", default="Courier New", help="Font name")
-def main(filename, exit_at, size, offset, dpi, figsize, fps, bins_min, bins_max, bins_step, fontname, filename_out):
+def main(filename, exit_at, by, dpi, figsize, fps, bins_min, bins_max, bins_step, fontname, filename_out):
     # convert figsize (string like "8,9" to a list of float [8.0, 9.0]
     figsize = figsize.split(",")
     figsize = map(float, figsize)
+
+    by_func = get_by_func(by)
 
     # Read CSV file to a Pandas DataFrame
     df_all = pd.read_csv(filename)
     df_all['Timestamp'] = pd.to_datetime(df_all['Timestamp'])
     df_all = df_all.set_index('Timestamp')
-    #df_all = df_all.iloc[-10000:,:]
+
     df_all.index = df_all.index.tz_localize('UTC').tz_convert('UTC')
 
-    # Get Numpy arrays from DataFrame
-    direction_all = df_all['direction'].values
-    var_all = df_all['speed'].values
-    index_all = df_all.index.to_datetime() #.values -> .to_datetime()
+    dt_start = df_all.index[0]
+    dt_end = df_all.index[-1]
+    print("First dt: %s" % dt_start)
+    print("Last  dt: %s" % dt_end)
+    td = dt_end - dt_start
+    print("      td: %s" % td)
+    Nslides = count(df_all, by_func)
+    print("  Slides: %d" % Nslides)
 
     # Define bins
     bins = np.arange(bins_min, bins_max, bins_step)
@@ -72,38 +104,28 @@ def main(filename, exit_at, size, offset, dpi, figsize, fps, bins_min, bins_max,
 http://www.github.com/scls19fr/windrose""")
     writer = FFMpegWriter(fps=fps, metadata=metadata)
 
-    dt0 = index_all[offset]
-    print("size: %d" % size)
-    print("offset: %d" % offset)
-    print("First dt: %s" % dt0)
-
     print("")
 
-    dt2 = None
-    i = 0
     with writer.saving(fig, filename_out, 100):
-        #for i in range(1000): # 100
         try:
-            while True: # loop until fails (end of data)
-                print("Processing %d" % (i + 1))
-                i1 = offset + i*size
-                i2 = offset + (i+1)*size + 1
-
-                index = index_all[i1:i2]
-                dt1 = index[0]
-                dt2 = index[-1]
+            for i, df in enumerate(generate(df_all, by_func)):
+                dt1 = df.index[0]
+                dt2 = df.index[-1]
                 td = dt2 - dt1
+                msg = """  Slide %s/%s
+    From %s
+    to %s
+    td %s
+""" % (i+1, Nslides, dt1, dt2, td)
+                print(msg)
                 title = "  From %s\n    to %s" % (dt1, dt2)
-                print(title)
-                print("    td %r" % td)
-                #print("    td %r" % td.astype('timedelta64[m]'))
     
                 try:
-                    direction = direction_all[i1:i2]
-                    var = var_all[i1:i2]
-
                     ax = WindroseAxes.from_ax(fig=fig) # scatter, bar, box, contour, contourf
-
+                    
+                    direction = df['direction'].values
+                    var = df['speed'].values
+                    
                     #ax.scatter(direction, var, alpha=0.2)
                     #ax.set_xlim([-bins[-1], bins[-1]])
                     #ax.set_ylim([-bins[-1], bins[-1]])
@@ -135,18 +157,13 @@ http://www.github.com/scls19fr/windrose""")
                 print("")
 
                 fig.clf()
-                i += 1
-                if i == exit_at - 1: # exit_at must be > 1
+                if i > exit_at - 1 and exit_at != 0: # exit_at must be > 1
                     break
         except KeyboardInterrupt:
             return
         except Exception as e:
             print(traceback.format_exc(), file=sys.stderr)
 
-        print("First dt: %r" % dt0)
-        print("Last  dt: %r" % dt2)
-        td = dt2 - dt0
-        print("      td: %r" % td.astype('timedelta64[D]'))
         N = i + 1
         print("Number of slides: %d" % N)
 

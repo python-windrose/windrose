2392b3eb8415f14180d29314031c15887cf195c4
scls19fr <s.celles@gmail.com>
2016-08-13 19:37:38 +0200

    Register projection windrose

--- windrose/windrose.py
+++ windrose/windrose.py
@@ -7,6 +7,7 @@ import locale
 import matplotlib as mpl
 import numpy as np
 from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
 from numpy.lib.twodim_base import histogram2d
 import matplotlib.pyplot as plt
 from pylab import poly_between
@@ -45,6 +46,7 @@ class WindroseAxes(PolarAxes):
     Create a windrose axes
 
     """
+    name = 'windrose'
 
     def __init__(self, *args, **kwargs):
         """
@@ -685,3 +687,6 @@ def plot_windrose_np(direction, var, kind='contour', clean_flag=True, by=None, r
         return ax
     else:
         raise(NotImplementedError("'by' keyword not supported for now https://github.com/scls19fr/windrose/issues/10"))
+
+
+register_projection(WindroseAxes)

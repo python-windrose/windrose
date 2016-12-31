ed3b7405edd6ec412ceafef99a1d54d88c58535d
kilojoules <jaq8@humboldt.edu>
2016-12-31 09:24:25 -0700

    pep8

--- samples/example_statistical_input.py
+++ samples/example_statistical_input.py
@@ -7,10 +7,10 @@ fig.add_axes(ax)
 windRose = np.loadtxt('amalia_directionally_averaged_speeds.txt')
 indexes = np.where(windRose[:, 1] > 0.1)
 windDirections = windRose[indexes[0], 0]
-windSpeeds = windRose[indexes[0], 1] * 2 / np.sqrt(np.pi) # convert from mean wind speed to weibull scale factor
+windSpeeds = windRose[indexes[0], 1] * 2 / np.sqrt(np.pi)  # convert from mean wind speed to weibull scale factor
 windFrequencies = windRose[indexes[0], 2]
-size  = len(windDirections)
+size = len(windDirections)
 ax.box(windDirections, windSpeeds, frequency=windFrequencies, mean_values=1, bins=[15, 18, 20, 23, 25], nsector=72)
-#ax.box(windDirections, [[windSpeeds[i], 2] for i in range(len(windSpeeds))], frequency=windFrequencies, weibull_factors=1, bins=[15, 18, 20, 23, 25], nsector=72)
+# ax.box(windDirections, [[windSpeeds[i], 2] for i in range(len(windSpeeds))], frequency=windFrequencies, weibull_factors=1, bins=[15, 18, 20, 23, 25], nsector=72)
 ax.set_yticklabels([])
 plt.show()

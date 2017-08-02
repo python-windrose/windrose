421b5b7ce81eca9a1bc1130c0a7c6b84a706d3f2
scls19fr <s.celles@gmail.com>
2017-08-02 20:28:33 +0200

    1.6.2 release

--- windrose/version.py
+++ windrose/version.py
@@ -1,7 +1,7 @@
 __author__ = 'Lionel Roubeyrie'
 __credits__ = ['Sebastien Celles']
 __email__ = 's.celles@gmail.com'
-__version__ = '1.6.1'
+__version__ = '1.6.2'
 __license__ = 'CeCILL-B'
 __url__ = 'https://github.com/python-windrose/windrose'
 __maintainer__ = "Sebastien Celles"

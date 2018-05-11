ad470b0d5e5be4161d20e12076dfc02b9483d9f7
Filipe Fernandes <ocefpaf@gmail.com>
2018-05-11 13:03:38 -0400

    remove all mentions of nose

--- setup.py
+++ setup.py
@@ -89,8 +89,8 @@ setup(
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require = {
-        'dev': ['check-manifest', 'nose'],
-        'test': ['coverage', 'nose'],
+        'dev': ['check-manifest', 'pytest'],
+        'test': ['coverage', 'pytest'],
         'advanced': ['pandas', 'scipy']
     },
 

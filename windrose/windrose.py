cc12c3646ba8da78f23b9fa0cf81757ddf531544
scls19fr <scls19fr@users.noreply.github.com>
2018-08-29 19:51:42 +0200

    Update windrose.py

--- windrose/windrose.py
+++ windrose/windrose.py
@@ -18,6 +18,7 @@ VAR_DEFAULT = 'speed'
 DIR_DEFAULT = 'direction'
 FIGSIZE_DEFAULT = (8, 8)
 DPI_DEFAULT = 80
+VF = np.vectorize(lambda wd: -wd + np.pi / 2)
 
 
 def _autogen_docstring(base):
@@ -735,6 +736,7 @@ def wrscatter(direction, var, ax=None, rmax=None, *args, **kwargs):
     Draw scatter plot
     '''
     ax = WindroseAxes.from_ax(ax, rmax=rmax)
+    direction = VF(direction)
     ax.scatter(direction, var, *args, **kwargs)
     return ax
 

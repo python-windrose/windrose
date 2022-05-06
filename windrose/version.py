a5bf1fd9e76858801620e8bb1094d3e46c37e8de
Filipe Fernandes <ocefpaf@gmail.com>
2022-05-06 13:21:58 -0300

    use modern pkg metadata

--- windrose/version.py
+++
@@ -1,7 +0,0 @@
-__author__ = "Lionel Roubeyrie"
-__credits__ = ["Sebastien Celles"]
-__email__ = "s.celles@gmail.com"
-__version__ = "1.6.8"
-__license__ = "CeCILL-B OR BSD-3-Clause"
-__url__ = "https://github.com/python-windrose/windrose"
-__maintainer__ = "Sebastien Celles"

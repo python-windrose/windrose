f2f0e66f241c494561b3806c1885ee1c1529a38b
Filipe Fernandes <ocefpaf@gmail.com>
2022-05-08 11:45:57 -0300

    pre-commit fixes

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,12 @@ sys.path.insert(0, os.path.abspath("../windrose"))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx",
-              "sphinx.ext.napoleon", "nbsphinx"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
